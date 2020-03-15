ddf450976682af70d21d27242928bf489e06ed62
SivenM <51032516+SivenM@users.noreply.github.com>
2020-03-15 15:12:27 +0300

    наши первые изменения

--- first_prog.py
+++ first_prog.py
@@ -2,13 +2,33 @@ import torch
 # Создание тензоров
 # Создаем тензор из нулей, где 3 = строки, 4 = столбцы
 x = torch.zeros([3, 4])
-print(x)
+#print(x)
 # Создаем тензор из единиц, где 3е число 4ка является количеством плоскотей
 y = torch.ones(3, 4, 4)
-print(y)
+#print(y)
 
 z = torch.Tensor([[1,  2,  3,  4],
                   [5,  6,  7,  8],
                   [9, 10, 11, 12]])
 print(z)
 print(z.shape)
+
+# Градиент
+a = torch.tensor(
+    [[1.,  2.,  3.,  4.],
+     [5.,  6.,  7.,  8.],
+     [9., 10., 11., 12.]])
+
+#######
+device = torch.device('cuda:0' 
+                      if torch.cuda.is_available() 
+                      else 'cpu')
+a = a.to(device)
+#######
+a.requires_grad_() #из константы делаем переменную, по кторой будем считать производную
+
+function = 10 * (a ** 2).sum()
+
+function.backward() # backward() считает производные фукции
+
+print(a.grad, '<- gradient')
